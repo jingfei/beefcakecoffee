045145da524972267d121a4ff214ebd3c8a566ca
jingfei <jingfei955047@gmail.com>
2017-02-21 17:13:19 +0800

    Improve home page animate effect.

--- js/home.js
+++ js/home.js
@@ -1,6 +1,8 @@
 var bgHeight;
+var disableTop = false;
 
 (function() {
+  window.scrollTo(0,0);
   bgHeight = $("#big-img").height();
   console.log(bgHeight);
 })();
@@ -9,18 +11,23 @@ $(window).bind("scroll", function() {
   var offset = $(document).scrollTop(),
       opacity = 0;
   console.log(offset);
+  if(disableTop) return;
   if(offset < bgHeight - 250) {
     document.getElementById("big-img").style.visibility = "visible";
     opacity = 1-offset/(bgHeight-250);
     $("#big-img").css("opacity",opacity);
     document.getElementsByClassName("navbar-default")[0].style.zIndex = "-1";
-  } else {
+  } else if (offset < bgHeight - 245) {
     document.getElementById("big-img").style.visibility = "hidden";
     document.getElementsByClassName("navbar-default")[0].style.zIndex = "100";
+  } else {
+    document.getElementById("big-img").style.display = "none";
+    document.getElementsByClassName("navbar-default")[0].style.marginTop = "0";
+    disableTop = true;
   }
 });
 
 function scrollToMain() {
   var body = $("html, body");
-  body.stop().animate({scrollTop: bgHeight-250}, '500', 'swing');
+  body.stop().animate({scrollTop: bgHeight-245}, '500', 'swing');
 }
