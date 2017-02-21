7f3d28751f89af30036841fb4a759ce6f04a6b96
jingfei <jingfei955047@gmail.com>
2017-02-21 16:22:17 +0800

    Add home page scroll effect.

--- js/home.js
+++ js/home.js
@@ -1,3 +1,26 @@
-(function() {
+var bgHeight;
 
+(function() {
+  bgHeight = $("#big-img").height();
+  console.log(bgHeight);
 })();
+
+$(window).bind("scroll", function() {
+  var offset = $(document).scrollTop(),
+      opacity = 0;
+  console.log(offset);
+  if(offset < bgHeight - 250) {
+    document.getElementById("big-img").style.visibility = "visible";
+    opacity = 1-offset/(bgHeight-250);
+    $("#big-img").css("opacity",opacity);
+    document.getElementsByClassName("navbar-default")[0].style.zIndex = "-1";
+  } else {
+    document.getElementById("big-img").style.visibility = "hidden";
+    document.getElementsByClassName("navbar-default")[0].style.zIndex = "100";
+  }
+});
+
+function scrollToMain() {
+  var body = $("html, body");
+  body.stop().animate({scrollTop: bgHeight-250}, '500', 'swing');
+}
