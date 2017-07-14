7be49cf1f92dcf21fcfbca8726ad7d27acdeea98
jingfei <jingfei955047@gmail.com>
2017-07-14 13:50:31 +0800

    Add home page effect.

--- js/home.js
+++ js/home.js
@@ -3,7 +3,8 @@ function scrollToMain() {
   body.stop().animate({scrollTop: bgHeight-245}, '500', 'swing');
 }
 
-const homeBacks = document.querySelectorAll('.homeBack');
+const homeBacks = document.querySelectorAll('.homeBack.pureBack');
+const homeBacksReverse = document.querySelectorAll('.homeBack.reverseBack');
 
 function checkBackground() {
   homeBacks.forEach(homeBack => {
@@ -18,5 +19,19 @@ function checkBackground() {
   });
 }
 
+function checkBackgroundReverse() {
+  homeBacksReverse.forEach(homeBack => {
+    const scrollTop = window.scrollY;
+    const scrollBottom = window.scrollY + window.innerHeight;
+    const isScrolled = scrollBottom > homeBack.offsetTop && scrollTop < homeBack.offsetTop + homeBack.offsetHeight;
+    if (isScrolled) {
+      let opacity = (scrollBottom - homeBack.offsetTop) / homeBack.offsetHeight;
+      opacity = opacity - .7 < 0 ? 0 : opacity - .7;
+      homeBack.firstElementChild.style.opacity = 1 - opacity * .2;
+    } 
+  });
+}
+
 window.addEventListener('scroll', checkBackground);
+window.addEventListener('scroll', checkBackgroundReverse);
 
